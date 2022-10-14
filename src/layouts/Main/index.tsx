8ada756c613256e8205115ce6d1838268afad80f
jung-il jeong <jji6665@cau.ac.kr>
2022-10-14 12:57:15 +0900

    add profile menu and advance routes

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -1,21 +1,47 @@
-import React, { lazy } from "react";
-import { Body, Header, Profile } from "./style";
-import { Route, Routes } from "react-router-dom";
+import React, {lazy, useCallback, useState} from "react";
+import {Body, Bottom, Header, LinkedButton, ProfileImg, ProfileMenu} from "./style";
+import {NavLink, redirect, Route, Routes,} from "react-router-dom";
+import gravatar from 'gravatar';
 
 const Apple = lazy(() => import("../../pages/Apple/Apple"));
+const Profile = lazy(() => import("../../pages/Profile/index"));
+
+const emailExample = 'jji6665@cau.ac.kr';
 
 const Main = () => {
+ const [showProfileMenu, setShowProfileMenu] = useState(false);
+
+ const onClickProfileImg = useCallback(()=> {
+  setShowProfileMenu((prevState)=>{
+   return prevState? false:true;
+  })
+ },[])
+ const onClickModifyProfile = useCallback(()=>{
+ },[]);
+ const onClickLogOut = useCallback(() => {}, []);
+
+
   return (
     <div>
       <Header>
-        <span>밥선배</span>
-        <Profile></Profile>
+       <span>밥선배</span>
+        <ProfileImg src={gravatar.url(emailExample, {s: '28px'})} onClick={onClickProfileImg}></ProfileImg>
+        {showProfileMenu && (<ProfileMenu>
+         <LinkedButton onClick={onClickModifyProfile}>프로필 수정</LinkedButton>
+         <LinkedButton onClick={onClickLogOut}>로그아웃</LinkedButton>
+        </ProfileMenu>)}
       </Header>
       <Body>
         <Routes>
-          <Route path={"/"} element={<Apple />} />
+         <Route path={'/profile'} element={<Profile />} />
+         <Route path={'/'} element={<Apple />} />
         </Routes>
       </Body>
+     <Bottom>
+      <div>홈</div>
+      <div>내 약속</div>
+      <div>검색</div>
+     </Bottom>
     </div>
   );
 };
--- src/layouts/Main/style.tsx
+++ src/layouts/Main/style.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled/macro";
 
 export const Header = styled.header`
   background-color: #23a1bd;
-  height: 40px;
+  height: 38px;
   position: fixed;
   width: 100%;
   text-align: center;
@@ -11,16 +11,56 @@ export const Header = styled.header`
   }
 `;
 
-export const Profile = styled.div`
+export const ProfileImg = styled.img`
   position: absolute;
   width: 28px;
   height: 28px;
   border-radius: 28px;
-  top: 5px;
+  top: 5.5px;
   right: 8px;
-  background-color: crimson;
 `;
 
 export const Body = styled.body`
   border-top: 40px;
+  height:4000px;
+`;
+
+export const LinkedButton = styled.div`
+  padding: 10px 20px;
+  border-radius: 6px;
+  font-size: 0.75em;
+  :hover {
+    background-color: slategrey;
+  }
+`;
+
+export const ProfileMenu = styled.div`
+  position: absolute;
+  top: 35px;
+  right: 30px;
+  diplay: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  
+  ${LinkedButton}
+`;
+
+
+export const Bottom = styled.div`
+  display: flex;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 35px;
+  background-color: #b870e8;
+  
+  & div {
+    min-width: 35px;
+    height: 100%;
+    text-align: center;
+    
+  }
 `;
