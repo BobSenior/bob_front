1474c363dc74a23a5134dc3018c1782d3e7a9e96
limenseop <dmstjq40344034@gmail.com>
2022-12-04 20:31:37 +0900

    appointment place구현 완료

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const fetcher = (url: string) => {
-  return axios("/api" + url)
+  return axios( url)
     .then((res) => res.data)
     .catch((error) => console.log(error));
 };
@@ -11,11 +11,11 @@ export const postFetcher = axios.create({});
 export const getFetcher = axios.create({});
 
 export const postDetailsFetcher = (url: string) => {
-  return axios("/api" + url).then((res) => res.data.result);
+  return axios(url).then((res) => res.data.result);
 };
 
 export const infiniteFetcher = (url: string) => {
-  return axios("/api" + url).then((res) => {
+  return axios(url).then((res) => {
     //TODO 배포 시 콘솔 삭제
     console.log(res.config.url);
     return res.data.result;
